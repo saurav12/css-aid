3101682f6bc6c209875f32b234a759df6f7310ff
Saurav Rastogi <saurav.rastogi25@gmail.com>
2017-07-09 20:21:25 +0530

    INITIAL commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+	<title>CSSAid</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div class="container">
+		<div class="hero-sec fx fx--ai-c fx--jc-c height--full-vh">
+			<div class="content text-center">
+				<h1>
+					<span class="boxed">CSS</span>
+					<span class="unboxed">Aid</span>
+				</h1>
+				<p class="lead">Developers won't write basic css properties again!</p>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
