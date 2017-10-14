0144468374096ff56d06ab3c1fc3b2e578cfbc8c
Saurav Rastogi <saurav.rastogi25@gmail.com>
2017-10-15 00:45:28 +0530

    ADDED structure to the page

--- index.php
+++ index.php
@@ -9,13 +9,50 @@
 </head>
 <body>
 	<div class="container">
-		<div class="hero-sec fx fx--ai-c fx--jc-c height--full-vh">
+		<div class="hero-sec fx fx--ai-c fx--jc-c">
 			<div class="content text-center">
 				<h1>
 					<span class="boxed">CSS</span>
 					<span class="unboxed">Aid</span>
 				</h1>
-				<p class="lead">Developers won't write basic css properties again!</p>
+				<p class="lead">Developers won't write the basic css properties again!</p>
+				<!-- <button class="btn social--btn-weibo">weibo</button> -->
+			</div>
+		</div>
+		<div class="desc--sec">
+			<div class="container">
+				<div class="desc--sec-body">
+					<div class="panel">
+						<div class="panel-heading">
+							<h5 class="panel-title">Getting Started</h5>
+						</div>
+						<div class="panel-body">
+							<p class="m--0">Copy-paste the stylesheet <code>&lt;link&gt;</code> into your <code>&lt;head&gt;</code> before all other stylesheets to load the CSS Aid library.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="desc--sec">
+			<div class="container">
+				<div class="desc--sec-header">
+					<h3 class="desc--sec-title text-upp">Margins</h3>
+					<p class="desc--sec-subtitle">Use margin utilities to quickly style the margins of an element. Great for any element.</p>
+				</div>
+				<div class="desc--sec-body">
+					<div class="panel">
+						<ul class="feature-list">
+							<li class="feature-list-item">
+								<div class="item-heading">Margin Top</div>
+								<div class="item-body">
+									<pre>
+										&lt;span class="mt--5"&gt;&lt;/span&gt;
+									</pre>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
